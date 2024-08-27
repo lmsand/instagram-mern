243d449bd6df1f5489d30934d6110d5966409187
lmsand <lmsand99@icloud.com>
2024-08-27 01:14:51 -0500

    cloudinary, datauri, middlewares: isAuthenticated, user.controller: editProfile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const register = async (req, res) => {
   try {
@@ -99,6 +101,43 @@ export const getProfile = async (req, res) => {
           user,
           success: true
       });
+  } catch (error) {
+      console.log(error);
+  }
+};
+
+
+export const editProfile = async (req, res) => {
+  try {
+      const userId = req.id;
+      const { bio, gender } = req.body;
+      const profilePicture = req.file;
+      let cloudResponse;
+
+      if (profilePicture) {
+          const fileUri = getDataUri(profilePicture);
+          cloudResponse = await cloudinary.uploader.upload(fileUri);
+      }
+
+      const user = await User.findById(userId).select('-password');
+      if (!user) {
+          return res.status(404).json({
+              message: 'User not found.',
+              success: false
+          });
+      };
+      if (bio) user.bio = bio;
+      if (gender) user.gender = gender;
+      if (profilePicture) user.profilePicture = cloudResponse.secure_url;
+
+      await user.save();
+
+      return res.status(200).json({
+          message: 'Profile updated.',
+          success: true,
+          user
+      });
+
   } catch (error) {
       console.log(error);
   }
