5f44b292b3ec5b914fbf9ab4ca0d713d95e9598f
lmsand <lmsand99@icloud.com>
2024-08-27 02:12:54 -0500

    user controller fix

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -69,7 +69,6 @@ export const login = async (req, res) => {
           bio: user.bio,
           followers: user.followers,
           following: user.following,
-          posts: populatedPosts
       }
       return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
           message: `Welcome back ${user.username}`,
